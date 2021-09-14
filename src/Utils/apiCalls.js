37fc55e7a56e187d07ced695a9d0d2c829b9b036
mendozalourdes <lourd.mendoza@gmail.com>
2021-09-14 16:01:03 -0500

    Create fetch call using 25 random images.

---
+++ src/Utils/apiCalls.js
@@ -0,0 +1,22 @@
+export const fetchNASAData = async () => {
+    let url = 'https://api.nasa.gov/planetary/apod?api_key=i7VC0jDS5LOoTBqSVLELbepXUTJqY77xUTn0fg9O&count=25';
+    let response = await fetch(url);
+    let data = await checkForErrors(response);
+    return data;
+  };
+
+  
+export const checkForErrors = response => {
+    if (response.status === 404) {
+      throw new Error('Oops, something went wrong. Please check back later.');
+    } else if (response.status === 500) {
+      throw new Error(
+        "Our servers seem to be down, please check back later!"
+      );
+    } else if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Oops, something went wrong. Please try again!');
+    }
+  };
+
