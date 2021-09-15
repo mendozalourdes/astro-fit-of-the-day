3af068e962389641c69b278972428bfa917b43cc
mendozalourdes <lourd.mendoza@gmail.com>
2021-09-15 18:58:51 -0500

    Create feature to like and unlike images.

--- src/Components/AllAstroCards/AllAstroCards.js
+++ src/Components/AllAstroCards/AllAstroCards.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import { Router, Link, NavLink } from 'react-router-dom';
 import SingleAstroCard from '../SingleAstroCard/SingleAstroCard';
+import { useState } from 'react';
+
 
 const AllAstroCards = ({astroData}) => {
-    console.log('allCards', astroData)
+    // console.log('allCards', astroData)
+
+    const [favorites, setFavorites] = useState([])
+
+    const addToFavorites = (id) => {
+        let newFavorite = astroData[id]
+        console.log("newFavorite", newFavorite)
+        console.log("asllAstroFavres", favorites) 
+        if (!favorites.includes(newFavorite)){
+          setFavorites([...favorites, newFavorite])  
+          console.log("updatedFavorites?", favorites)
+        } else {
+          return null
+        }
+      }
+
+      const removeFromFavorites = (className) => {
+          console.log("removooooveee", className)
+          const allFaves = favorites
+          let remainingFavorites = allFaves.filter( fave => {
+              console.log("faveeeee", fave)
+              if(!fave.title.includes(className)) {
+                  return fave
+              }
+          })
+
+        //   let remainingFavorites = favorites.filter(fave => fave.includes(className))
+          console.log("remain", remainingFavorites)
+        // let remainingFavorites = favorites.filter((data, index) => id !== index )
+        setFavorites([...remainingFavorites])
+        console.log("removed?", favorites)
+        } 
+      
+
+
 
     const allAstroData = astroData.map((data, i) => {
         return (
             <SingleAstroCard 
+                addToFavorites={addToFavorites}
+                removeFromFavorites={removeFromFavorites}
                 id={i}
                 key={i}
                 date={data.date}
@@ -15,7 +53,7 @@ const AllAstroCards = ({astroData}) => {
                 hdurl={data.hdurl}
                 title={data.title}
                 url={data.url} 
-                button={true}           
+                buttonStyle={true}           
             />
             
         )
--- src/Components/SingleAstroCard/SingleAstroCard.js
+++ src/Components/SingleAstroCard/SingleAstroCard.js
@@ -3,42 +3,71 @@ import { Router, Link, NavLink } from 'react-router-dom';
 import { useState } from 'react';
 import './SingleAstroCard.css'
 
-const SingleAstroCard = ({id, key, date, button, explanation, hdurl, title, url}) => {
+const SingleAstroCard = ({addToFavorites, removeFromFavorites, id, key, date, buttonStyle, explanation, hdurl, title, url}) => {
+  
     
     const [isDisabled, setIsDisabled] = useState(false);
-    // const [canFavorite, setCanFavorite] = useState(true);
+    const [canFavorite, setCanFavorite] = useState(true);
+    const [favorites, setFavorites] = useState([])
 
-    const handleClick = (e) => {
-        setIsDisabled(true);
-        // setCanFavorite(addToFavorites(e))
+    const [isLiked, updateLike] = useState(false);
+
+
+    // const handleLike = () => {
+    //     // let currentLikedImages = 
+    //     if (!isLiked) {
+    //       updateLike(true);
+    //       if (!currentLikedBands.includes(name))
+    //         props.updateLikedBands(
+    //           [...currentLikedBands, name]
+    //         );
+    //     } else {
+    //       updateLike(false);
+    //       if (currentLikedBands.includes(name))
+    //         props.updateLikedBands(
+    //           currentLikedBands
+    //           .filter(band => band !== name)
+    //           );
+    //     }
+    //   };
+
+    const handleLike = (e) => {
+        console.log("event", e.target.id)
+        if(!isLiked) {
+            updateLike(true);
+            addToFavorites(e.target.id)
+        } else {
+            updateLike(false);
+            removeFromFavorites(e.target.classList)
+
+        }
     }
 
-    let buttonImg = button ?
+    let buttonImg = buttonStyle ?
                    <img id={id} key={key} src="https://img.icons8.com/office/30/000000/like--v1.png" alt="favorite button"/>
                  : <img id={id} src="https://img.icons8.com/office/30/000000/delete-sign.png" alt="delete button"/>;
 
 
     return (
-        <NavLink to={`/astro-info/${id}`}>
+        // <NavLink to={`/astro-info/${id}`}>
         <div className="each-astro-data" id={id}>
             {/* <p>{url}</p> */}
             <a href={url}>Share This Image</a>
             <p className="astro-title"> {title}</p>
-            <button id={id} 
-            disabled={isDisabled}
-            // onClick={e => {
-            //     if(button) {
-            //         handleClick(e)
-            //     } else {
-            //         removeFromFavorites(e)
-            //     }
-            //     }}
-            > {buttonImg} </button>
+            <NavLink to={`/astro-info/${id}`}><button className="more-info-btn">More Info</button></NavLink>
+        
             
+            <button id={id} className={title} disabled={isLiked} onClick={e => {handleLike(e)}}>
+            <i className="like-image" id={id} aria-hidden="true"> ❤️</i>
+        </button>
+        <button id={id} className={title} disabled={!isLiked} onClick={e => {handleLike(e)}}>
+        <i className={title} id={id} aria-hidden="true">💔</i>
+        </button>
+        <p>You {isLiked ? "liked" : "have not liked"} this image. </p>
             <img src={url} alt={title} className="astro-image"/>
 
         </div>
-        </NavLink>
+        // </NavLink>
     );
 };
 
